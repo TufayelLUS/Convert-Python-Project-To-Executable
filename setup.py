384be3e5d1c3fe6f392be5eb98dca39455ef08c4
Tufayel Ahmed <39314838+TufayelLUS@users.noreply.github.com>
2024-12-13 15:46:48 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import sys
 from cx_Freeze import setup, Executable
 
+# pip install cx_freeze==7.2.4
+
 base = None
 
 if sys.platform == "win32":
@@ -15,6 +17,7 @@ options = {
         "optimize": 2, # set level of optimization 0-2
         "packages": [],  # List of packages to include
         "include_files": [],  # List of additional files to include
+        "excludes": [] # List of packages to exclude
     },
 }
 
